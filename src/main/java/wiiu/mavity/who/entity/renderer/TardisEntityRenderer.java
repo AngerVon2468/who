8aea37f80978c646a8382aedfc65dc5538147810
AngerVon2468 <thomaschad0909@gmail.com>
2024-07-13 16:07:10 +1000

    Kotlin Language Adapter for hard failure warnings, updated tardis entity renderer for fancy code

--- src/main/java/wiiu/mavity/who/entity/renderer/TardisEntityRenderer.java
+++ src/main/java/wiiu/mavity/who/entity/renderer/TardisEntityRenderer.java
@@ -2,10 +2,14 @@ package wiiu.mavity.who.entity.renderer;
 
 import net.fabricmc.api.*;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.entity.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationAxis;
 
 import org.jetbrains.annotations.NotNull;
@@ -24,13 +28,36 @@ public class TardisEntityRenderer extends EntityRenderer<TardisEntity> {
         this.entityModel = new TardisEntityModel<>(ctx.getPart(TardisEntityModel.LAYER_LOCATION));
     }
 
-    // TODO: Use tardis.getHorizontalFacing to change the model rotation properly.
     @Override
     public void render(@NotNull TardisEntity tardis, float yaw, float tickDelta, @NotNull MatrixStack matrices, @NotNull VertexConsumerProvider vertexConsumers, int light) {
         super.render(tardis, yaw, tickDelta, matrices, vertexConsumers, light);
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(tardis.getYaw()));
+
+        // Directional Facing
+        if (tardis.getHorizontalFacing() == Direction.NORTH || tardis.getHorizontalFacing() == Direction.SOUTH) {
+
+            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(tardis.getYaw()));
+
+        } else if (tardis.getHorizontalFacing() == Direction.EAST || tardis.getHorizontalFacing() == Direction.WEST) {
+
+            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-tardis.getYaw()));
+
+        }
+
+        // Fix for accidental y axis inversion.
         matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180f));
         matrices.translate(0, -1.5, 0);
+
+        // Off-ground rotation movement
+        BlockPos pos = tardis.getBlockPos().offset(Direction.DOWN, 1);
+        BlockState state = tardis.getWorld().getBlockState(pos);
+        if (state.isOf(Blocks.AIR)) {
+            double offset = Math.sin((tardis.getWorld().getTime() + tickDelta) / 8.0) / 4.0;
+            // Move the entity
+            matrices.translate(0, -1 - offset, 0);
+            // Rotate the entity
+            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees((tardis.getWorld().getTime() + tickDelta) * 4));
+        }
+
         VertexConsumer vertexConsumer = vertexConsumers.getBuffer(this.entityModel.getLayer(getTexture(tardis)));
         this.entityModel.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, 1.0f, 1.0f, 1.0f, 1.0f);
     }
